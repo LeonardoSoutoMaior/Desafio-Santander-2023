176aa4be8d8b000b669873c3111cdd23a5f13f1a
Leonardo <leo.soutomaior@hotmail.com>
2023-09-21 17:45:31 -0300

    Initial commit

---
+++ src/main/java/com/example/DesafioSantander2023/DesafioSantander2023Application.java
@@ -0,0 +1,13 @@
+package com.example.DesafioSantander2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSantander2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSantander2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DesafioSantander2023/DesafioSantander2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DesafioSantander2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSantander2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
